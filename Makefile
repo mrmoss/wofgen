adb77e2df81954009d96811a8c88adf73784ef45
Mike Moss <mrmoss@alaska.edu>
2016-03-05 19:05:55 -0900

    Added ufw.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CXX=g++
 OPTS=-O
 CFLAGS=$(OPTS) -Wall
 
-all: wof_iptables wof_netsh wof_pf
+all: wof_iptables wof_netsh wof_pf wof_ufw
 
 wof_iptables: wof.cpp iptables.cpp
 	$(CXX) $(CFLAGS) $^ -o $@
@@ -13,7 +13,11 @@ wof_netsh: wof.cpp netsh.cpp
 wof_pf: wof.cpp pf.cpp
 	$(CXX) $(CFLAGS) $^ -o $@
 
+wof_ufw: wof.cpp ufw.cpp
+	$(CXX) $(CFLAGS) $^ -o $@
+
 clean:
 	- rm -f wof_iptables wof_iptables.exe
 	- rm -f wof_netsh wof_netsh.exe
 	- rm -f wof_pf wof_pf.exe
+	- rm -f wof_ufw wof_ufw.exe
---
+++ ufw.cpp
@@ -0,0 +1,50 @@
+#include <string>
+
+std::string pre_rules()
+{
+	std::string pre;
+	pre+="ufw --force disable\n";
+	pre+="ufw --force reset\n";
+	pre+="ufw logging on\n";
+	pre+="ufw default deny incoming\n";
+	pre+="ufw default deny outgoing\n";
+	return pre;
+}
+
+std::string post_rules()
+{
+	return "ufw --force enable\n";
+}
+
+std::string gen_rule(std::string proto,
+	std::string l_ip,std::string l_mask,std::string l_port,
+	std::string dir,
+	std::string f_ip,std::string f_mask,std::string f_port,
+	std::string action,
+	bool V6)
+{
+	std::string rule;
+	rule+="ufw ";
+	if(action=="deny")
+		rule+="block";
+	else
+		rule+="allow";
+	std::string dir_str=" out";
+	if(dir=="<")
+	{
+		dir_str=" in ";
+		std::swap(l_ip,f_ip);
+		std::swap(l_mask,f_mask);
+		std::swap(l_port,f_port);
+	}
+	rule+=dir_str;
+	rule+=" proto "+proto;
+	rule+=" from "+l_ip+"/"+l_mask;
+	if(l_port!="0")
+		rule+=" port "+l_port;
+	rule+=" to "+f_ip+"/"+f_mask;
+	if(f_port!="0")
+		rule+=" port "+f_port;
+
+	return rule;
+}
